9cdb2263c045fc4426a636e95c0701fc2fe70dfe
Brad Taylor <bradtaylor707@gmail.com>
2015-02-17 17:49:06 -0800

    Added pictures

--- Puzzle 15/ViewController.m
+++ Puzzle 15/ViewController.m
@@ -41,6 +41,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    [self.view setBackgroundColor:[UIColor blackColor]];
 
 }
 - (IBAction)didTapShuffleButton:(UIButton *)sender {
