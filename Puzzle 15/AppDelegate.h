e09f0c0fceb96ca85b7459a718eadaa9e2e1aaf4
Brad Taylor <bradtaylor707@gmail.com>
2015-02-17 16:05:21 -0800

    Initial Commit

---
+++ Puzzle 15/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Puzzle 15
+//
+//  Created by Brad Taylor on 2/17/15.
+//  Copyright (c) 2015 Brad Taylor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Puzzle 15/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Puzzle 15
+//
+//  Created by Brad Taylor on 2/17/15.
+//  Copyright (c) 2015 Brad Taylor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
