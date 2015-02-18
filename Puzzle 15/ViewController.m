873a321267a0cb0041bb1f6d1f92680e7c7f3430
Brad Taylor <bradtaylor707@gmail.com>
2015-02-17 16:27:13 -0800

    Initial commit, layout is laid out

--- Puzzle 15/ViewController.m
+++ Puzzle 15/ViewController.m
@@ -8,7 +8,31 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () {
+    
+}
+
+@property (weak, nonatomic) IBOutlet UIButton *oneTile;
+@property (weak, nonatomic) IBOutlet UIButton *twoTile;
+@property (weak, nonatomic) IBOutlet UIButton *threeTile;
+@property (weak, nonatomic) IBOutlet UIButton *fourTile;
+@property (weak, nonatomic) IBOutlet UIButton *fiveTile;
+@property (weak, nonatomic) IBOutlet UIButton *sixTile;
+@property (weak, nonatomic) IBOutlet UIButton *sevenTile;
+@property (weak, nonatomic) IBOutlet UIButton *eightTile;
+@property (weak, nonatomic) IBOutlet UIButton *nineTile;
+@property (weak, nonatomic) IBOutlet UIButton *tenTile;
+@property (weak, nonatomic) IBOutlet UIButton *elevenTile;
+@property (weak, nonatomic) IBOutlet UIButton *twelveTile;
+@property (weak, nonatomic) IBOutlet UIButton *thirteenTile;
+@property (weak, nonatomic) IBOutlet UIButton *fourteenTile;
+@property (weak, nonatomic) IBOutlet UIButton *fifteenTile;
+@property (weak, nonatomic) IBOutlet UIButton *blankTile;
+
+@property (weak, nonatomic) IBOutlet UIButton *resetButton;
+@property (weak, nonatomic) IBOutlet UIButton *shuffleButton;
+@property (weak, nonatomic) IBOutlet UISlider *shuffleSlider;
+
 
 @end
 
@@ -17,6 +41,19 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+
+}
+- (IBAction)didTapShuffleButton:(UIButton *)sender {
+    NSLog(@"Tapped the %@ button.", [sender currentTitle]);
+}
+- (IBAction)didTapResetButton:(UIButton *)sender {
+    NSLog(@"Tapped the %@ button.", [sender currentTitle]);
+}
+- (IBAction)didTapTileButton:(UIButton *)sender {
+    NSLog(@"Tapped the %@ button.", [sender currentTitle]);
+}
+- (IBAction)didMoveSlider:(UISlider *)sender {
+    NSLog(@"Slider is now at %@", @([self.shuffleSlider value]));
 }
 
 - (void)didReceiveMemoryWarning {
